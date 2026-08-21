f480c51e7951cc1a31b371241776b69d473f3f2b
prashankulathunga <prashanrandikakulathunga@gmail.com>
2026-08-21 20:32:36 +0530

    inizialize commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App () {
+    return (
+        <div className='text-sm md:text-base'>
+            Hello how are you this platform is a women IT.
+        </div>
+    )
+}
+
+export default App
