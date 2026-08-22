9b2333ea7468f0e790feb2149af47759c8ac6e23
prashankulathunga <prashanrandikakulathunga@gmail.com>
2026-08-22 13:46:16 +0530

    create core folders and files arc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App () {
     return (
-        <div className='text-sm md:text-base'>
+        <div className='text-sm md:text-base text-center mt-12'>
             Hello how are you this platform is a women IT.
+            This is new version of education system
         </div>
     )
 }
